e3bff6e78bcd8c9ee210fc13922291b177734f34
Joanne Chen <joannechen@Joannes-MacBook-Air.local>
2024-05-27 11:13:34 +0100

    main menu and list set with warnings

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import { Grid, Tooltip, IconButton } from '@material-ui/core';
+import { Grid, Tooltip, IconButton, Menu, MenuItem } from '@material-ui/core';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import MenuBookOutlinedIcon from '@mui/icons-material/MenuBookOutlined';
 import MusicNoteOutlinedIcon from '@mui/icons-material/MusicNoteOutlined';
 import './styles.css';
 import PracticePage from "./pages/PracticePage";
+import { useState } from 'react';
 
 const pageIconButtons = [
   {pageTitle: 'Home', buttonIcon: <HomeOutlinedIcon style={{color: 'white'}} fontSize='large'/>, pageControl: 'Home'},
-  {pageTitle: 'Study', buttonIcon: <MenuBookOutlinedIcon style={{color: 'white'}} fontSize='large'/>, pageControl: 'Study'},
-  {pageTitle: 'Practice', buttonIcon: <MusicNoteOutlinedIcon style={{color: 'white'}} fontSize='large' />, pageControl: 'Practice'},
-
+  {pageTitle: 'Learn and study', buttonIcon: <MenuBookOutlinedIcon style={{color: 'white'}} fontSize='large'/>, pageControl: 'Study'},
+  {pageTitle: 'Play and practise', buttonIcon: <MusicNoteOutlinedIcon style={{color: 'white'}} fontSize='large' />, pageControl: 'Practice'},
 ]
 
+const studyList = ['study 1', 'study 2', 'study 3']
+const practiseList = ['practice 1', 'practice 2', 'practice 3']
+
 function App() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [menuList, setMenuList] = useState([])
+  const open = Boolean(anchorEl);
+
+
+  const handleRedirect = (page) => {
+    console.log('go to page', page)
+  }
+
+  const handleClick = (event, pageControl) => {
+    if(pageControl === 'Study' || pageControl === 'Practice') {
+      const findMenuList = pageControl === 'Study' ? studyList : practiseList
+      setMenuList(findMenuList)
+      setAnchorEl(event.currentTarget);
+    } else {
+      handleRedirect(pageControl)
+      setMenuList([])
+    }
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
       <div className="appHeader"> 
@@ -21,19 +46,71 @@ function App() {
             <h2 className='appTitle'>The Visual Violinist</h2>
           </Grid>
           <Grid item >
-            {pageIconButtons.map(item => (
-              <Tooltip title={item.pageTitle}>
-                <IconButton
-                  onClick={() => null}
-                  // size="medium"
-                  // sx={{ ml: 2 }}
-                  // aria-controls={open ? 'account-menu' : undefined}
-                  // aria-haspopup="true"
-                  // aria-expanded={open ? 'true' : undefined}
+            {pageIconButtons.map((item, i) => (
+              <>
+                <Tooltip title={item.pageTitle} key={`menu-tooltip-${i}`}>
+                  <IconButton
+                    key={`menu-button-${i}`}
+                    onClick={ e => handleClick(e, item.pageControl)}
+                    size="medium"
+                    sx={{ ml: 2 }}
+                    aria-controls={open ? item.pageControl : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                  >
+                    {item.buttonIcon}
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  key={`menu-drawer-${i}`}
+                  getContentAnchorEl={null}
+                  anchorEl={anchorEl}
+                  id={item.pageControl}
+                  open={open}
+                  onClose={handleClose}
+                  onClick={handleClose}
+                  PaperProps={{
+                    elevation: 0,
+                    sx: {
+                      overflow: 'visible',
+                      filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                      mt: 1.5,
+                      '& .MuiAvatar-root': {
+                        width: 32,
+                        height: 32,
+                        ml: -0.5,
+                        mr: 1,
+                      },
+                      '&::before': {
+                        content: '""',
+                        display: 'block',
+                        position: 'absolute',
+                        top: 0,
+                        right: 14,
+                        width: 10,
+                        height: 10,
+                        bgcolor: 'background.paper',
+                        transform: 'translateY(-50%) rotate(45deg)',
+                        zIndex: 0,
+                      },
+                    },
+                  }}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
                 >
-                  {item.buttonIcon}
-                </IconButton>
-              </Tooltip>
+                  {menuList.map(course => (
+                    <MenuItem onClick={handleClose} key={`menulist-${course}`}>
+                      {course}
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </>              
             ))}
           </Grid>
         </Grid>
