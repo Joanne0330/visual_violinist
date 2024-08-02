314205974e8ff36c34daab4c7defd9094889d6a0
Joanne Chen <joannechen@Joannes-MacBook-Pro.local>
2024-08-02 11:46:09 +0100

    loading implemented for Learn section for better visual

--- src/common/LearnSession/index.js
+++ src/common/LearnSession/index.js
@@ -4,64 +4,89 @@ import './styles.css';
 import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 import MusicNoteOutlinedIcon from '@mui/icons-material/MusicNoteOutlined';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import MusicLoader from '../Loading';
 
 const LearnSession = (props) => {
 	const {dataArray, dataIndex, setDataIndex, isSmallScreen, isMediumScreen, pathname, keyName} = props;
+	const [isLoading, setIsLoading] = useState(false);
 	let navigate = useNavigate();
+
+	const toNextSlide = () => {
+		setDataIndex(dataIndex + 1);
+		setIsLoading(true);
+		setTimeout(() => 
+			setIsLoading(false)
+		, 300);
+	}
+
+	const toPreviouSlide = () => {
+		setDataIndex(dataIndex - 1);
+		setIsLoading(true);
+		setTimeout(() => 
+			setIsLoading(false)
+		, 300);
+	}
+
 
 	return (
 		<div style={{padding: 20, display: 'flex', justifyContent: 'center'}}>
 			<Card sx={{ width: isSmallScreen && !isMediumScreen ? '100%' : isMediumScreen ? '80%' : '60%',  borderRadius:'15px' }}>
-				{!!dataArray[dataIndex].learnImg &&
-					<CardMedia
-					sx={{ minHeight: isSmallScreen && !isMediumScreen ? 250 : isMediumScreen ? 350 : 450}}
-					image={dataArray[dataIndex].learnImg}
-					title={dataArray[dataIndex].imgTitle}
-					/>
-				}
-				<CardContent className="learnCardContent" >
-						<Grid container  direction="column" spacing={2} justifyContent='center' alignItems='center'>
-							{dataArray[dataIndex].texts.map((text, id) => (
-								<Grid item xs={12} key={id}> 
-									<Typography variant="body1" className="contentText" >{text}</Typography>
-								</Grid>
-							))}
-							<Grid container direction="row" justifyContent='space-between'>
-								<IconButton 
-									disabled={dataIndex === 0} 
-									color={dataIndex === 0 ? 'default' : 'secondary'} 
-									onClick={() => setDataIndex(dataIndex - 1)} 
-									style={{margin: '12px'}}
-									>
-									<KeyboardDoubleArrowLeftIcon  fontSize="large"/>
-								</IconButton>
-								{pathname !== '/learn/introduction' && dataIndex === dataArray.length - 1 ?
-									<Tooltip 
-										title={<Fragment><Typography color="inherit">{`Practice ${keyName}`}</Typography></Fragment>} 
-										arrow
-									>
-										<IconButton color='secondary' style={{margin: '12px'}} onClick={() => navigate(pathname.replace('learn', 'practice'))}>
-											<MusicNoteOutlinedIcon fontSize='large'  />
+				{isLoading ? 
+					<MusicLoader /> 
+				:
+					<>
+						{!!dataArray[dataIndex].learnImg &&
+							<CardMedia
+							sx={{ minHeight: isSmallScreen && !isMediumScreen ? 250 : isMediumScreen ? 350 : 450}}
+							image={dataArray[dataIndex].learnImg}
+							title={dataArray[dataIndex].imgTitle}
+							/>
+						}
+						<CardContent className="learnCardContent" >
+								<Grid container  direction="column" spacing={2} justifyContent='center' alignItems='center'>
+									{dataArray[dataIndex].texts.map((text, id) => (
+										<Grid item xs={12} key={id}> 
+											<Typography variant="body1" className="contentText" >{text}</Typography>
+										</Grid>
+									))}
+									<Grid container direction="row" justifyContent='space-between'>
+										<IconButton 
+											disabled={dataIndex === 0} 
+											color={dataIndex === 0 ? 'default' : 'secondary'} 
+											onClick={toPreviouSlide} 
+											style={{margin: '12px'}}
+											>
+											<KeyboardDoubleArrowLeftIcon  fontSize="large"/>
 										</IconButton>
-									</Tooltip>
-									:
-									<IconButton 
-										disabled={dataIndex === dataArray.length - 1} 
-										color={dataIndex === dataArray.length - 1 ? 'default' : 'secondary'}
-										onClick={() => setDataIndex(dataIndex + 1)}
-										style={{margin: '12px'}}
-										>
-										<KeyboardDoubleArrowRightIcon  fontSize="large"/>
-									</IconButton>
-								}
-							</Grid>
-							<Grid item xs={12}>
-								<Pagination size="small" count={dataArray.length} page={dataIndex + 1} variant="outlined" color="secondary" hidePrevButton hideNextButton/>
-							</Grid>
-						</Grid>
-				</CardContent>
+										{pathname !== '/learn/introduction' && dataIndex === dataArray.length - 1 ?
+											<Tooltip 
+											title={<Fragment><Typography color="inherit">{`Practice ${keyName}`}</Typography></Fragment>} 
+											arrow
+											>
+												<IconButton color='secondary' style={{margin: '12px'}} onClick={() => navigate(pathname.replace('learn', 'practice'))}>
+													<MusicNoteOutlinedIcon fontSize='large'  />
+												</IconButton>
+											</Tooltip>
+											:
+											<IconButton 
+											disabled={dataIndex === dataArray.length - 1} 
+											color={dataIndex === dataArray.length - 1 ? 'default' : 'secondary'}
+											onClick={toNextSlide}
+											style={{margin: '12px'}}
+											>
+												<KeyboardDoubleArrowRightIcon  fontSize="large"/>
+											</IconButton>
+										}
+									</Grid>
+									<Grid item xs={12}>
+										<Pagination size="small" count={dataArray.length} page={dataIndex + 1} variant="outlined" color="secondary" hidePrevButton hideNextButton/>
+									</Grid>
+								</Grid>
+						</CardContent>
+					</>
+				}
 			</Card>
 		</div>
 	)
--- src/common/Loading/index.js
+++ src/common/Loading/index.js
@@ -2,13 +2,13 @@ import React, { Fragment } from 'react';
 import musicLoader from './musicLoader.gif';
 
 export const MusicLoader = () => (
-    <Fragment>
-        <img 
-            src={musicLoader}
-            style={{ width: '200px', margin: 'auto', display: 'block' }}
-            alt='Loading...'
-        />
-    </Fragment>
+	<Fragment>
+		<img 
+			src={musicLoader}
+			style={{ width: '100%', margin: 'auto', display: 'block', background: 'linear-gradient(75deg, black, #254d74)' }}
+			alt='Loading...'
+		/>
+	</Fragment>
 );
 
 export default MusicLoader; 
